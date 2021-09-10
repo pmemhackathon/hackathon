e3761c93082c9d1a78dd9c99a6f09ef8c5ac447c
Łukasz Stolarczuk <lukasz.stolarczuk@intel.com>
2021-09-10 09:35:50 +0200

    example B: modify run_*.sh to set kvfile path only once

--- examples/B/run_py.sh
+++ examples/B/run_py.sh
@@ -3,20 +3,22 @@
 # shell commands to run the Python version of this example
 #
 
+my_kvfile=/pmem/kvfile
+
 # remove file if left over from previous run
-rm -f /pmem/kvfile
+rm -f $my_kvfile
 
 # add some values
-./kv.py /pmem/kvfile bach 1685-1750
-./kv.py /pmem/kvfile mozart 1756-1791
+./kv.py $my_kvfile bach 1685-1750
+./kv.py $my_kvfile mozart 1756-1791
 
 # print a value
-./kv.py /pmem/kvfile bach
+./kv.py $my_kvfile bach
 
 # add some more
-./kv.py /pmem/kvfile beethoven 1770-1827
-./kv.py /pmem/kvfile brahms 1833-1897
-./kv.py /pmem/kvfile haydn 1732-1809
+./kv.py $my_kvfile beethoven 1770-1827
+./kv.py $my_kvfile brahms 1833-1897
+./kv.py $my_kvfile haydn 1732-1809
 
 # print all k-v pairs in kvfile
-./kv.py /pmem/kvfile
+./kv.py $my_kvfile
